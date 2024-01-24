dec83ba7814d4a3312a32285915a0575d71ed9d8
kilian-nagel <nagelkilian05@gmail.com>
2024-01-24 14:49:13 +0100

    refactor: mise en place de fonctions pour construire les sous-menus

--- scripts/menu-content.js
+++ scripts/menu-content.js
@@ -1,11 +1,120 @@
-const biographieSubmenu = '<div class="menu"><div class="content"><div class="container"><div class="flex gap-2"><img src="/media/icones/tools.svg" alt=""><div><h3 class="fs-0">Compétences</h3><p class="text fs-0">Mes compétences techniques et interpersonnelles</p></div></div></div><div class="container"><div class="flex gap-2"><img src="./media/icones/flag.svg" alt=""><div><h3 class="fs-0">Parcours</h3><p class="text fs-0">Mon parcours scolaire</p></div></div></div><div class="container"><div class="flex gap-2"><img src="./media/icones/heart.svg" alt=""><div><h3 class="fs-0">Centre d\'intérêts</h3><p class="text fs-0">Mes activités extrascolaires</p></div></div></div></div></div>';
+const biographieData = [
+    {
+        title:"Compétences",
+        text:"Mes compétences techniques et interpersonnelles"
+    },
+    {
+        title:"Mon parcours",
+        text:"Mon parcours scolaire"
+    },
+    {
+        title:"Centres d'întérêts",
+        text:"Mes activités extrascolaires"
+    }
+];
 
-const contactSubmenu = '<div class="menu"><div class="content"><div class="container"><div class="flex gap-2"><img src="/media/icones/tools.svg" alt=""><div><h3 class="fs-0">Mail</h3><p class="text fs-0">Contacter par mail</p></div></div></div><div class="container"><div class="flex gap-2"><img src="./media/icones/flag.svg" alt=""><div><h3 class="fs-0">Télephone</h3><p class="text fs-0">Contacter par téléphone</p></div></div></div><div class="container"><div class="flex gap-2"><img src="./media/icones/heart.svg" alt=""><div><h3 class="fs-0">Réseaux</h3><p class="text fs-0">Mes réseaux</p></div></div></div></div></div>';
+const contactData = [
+    {
+        title:"Mail",
+        text:"Contacter par mail"
+    },
+    {
+        title:"Télephone",
+        text:"Contacter par téléphone"
+    },
+];
 
-const projetsSubmenu = '<div class="menu"><div class="content"><div class="container"><div class="flex gap-2"><img src="/media/icones/tools.svg" alt=""><div><h3 class="fs-0">Scolaire</h3><p class="text fs-0">Projets dans le cadre scolaire</p></div></div></div><div class="container"><div class="flex gap-2"><img src="./media/icones/flag.svg" alt=""><div><h3 class="fs-0">Personnel</h3><p class="text fs-0">Projets personnels</p></div></div></div></div></div>';
+const projectsData = [
+    {
+        title:"Scolaires",
+        text:"Projets scolaires"
+    },
+    {
+        title:"Personnels",
+        text:"Projets personnels"
+    },
+];
 
 const menuContent = {
-    "lien-biographie":biographieSubmenu,
-    "lien-contact":contactSubmenu,
-    "lien-projets":projetsSubmenu
+    "lien-biographie":biographieData,
+    "lien-contact":contactData,
+    "lien-projets":projectsData
 }
+
+function buildSubmenuContent(data){
+    const root = buildElement("div",{"className":"menu"});
+    const content = buildElement("div",{"className":"content"});
+
+    for(let submenuData of data){
+        const submenu = buildSubmenuContainer(submenuData);
+        content.appendChild(submenu);
+    }
+
+    root.appendChild(content);
+    return root;
+}
+
+function buildSubmenuContainer(data){
+    const container = buildElement("div",{"className":"container"});
+    const title = buildElement("h3",{"className":"fs-0","text":data.title});
+    const text = buildElement("p",{"className":"text fs-0","text":data.text});
+
+    container.appendChild(title);
+    container.appendChild(text);
+    return container;
+}
+
+function buildElement(elementType,data){
+    const element = document.createElement(elementType);
+    if(data.hasOwnProperty("className") && !isStringEmpty(data.className) && isString(data.className)){
+        element.className = data.className;
+    }
+    if(data.hasOwnProperty("text") && !isStringEmpty(data.className) && isString(data.className)){
+        element.textContent = data.text;
+    }
+    return element;
+}
+
+function popDirectory(path){
+    var path = window.location.pathname;
+    var pathArray = path.split('/');
+    pathArray.pop();
+    var newPath = pathArray.join('/');
+    newPath += "/";
+    return newPath;
+}
+
+function resolveFilepath(fileName){
+    let curPath = getCwdPath();
+    if(curPath.includes("pages")){
+        if(fileName=="index.html"){
+            curPath = popDirectory(curPath);
+            console.log(curPath);
+            curPath += "index.html";
+        } else {
+            curPath = curPath + fileName;
+        }
+    } else {
+        if(fileName==="index.html"){
+            curPath = curPath + fileName;
+        } else {
+            curPath = popDirectory(curPath);
+            curPath = curPath + `pages/${fileName}`;
+        }
+    }
+    return curPath;
+}
+
+function getCwdPath() {
+    return popDirectory(window.location.pathname);
+}
+
+function isStringEmpty(str){
+    return str === "";
+}
+
+function isString(str){
+    return typeof str === 'string' || str instanceof String;
+}
+
+console.log(resolveFilepath("index.html"));
