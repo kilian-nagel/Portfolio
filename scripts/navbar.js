ea8b4430c631f510c977b1a1e330f2bf6ca38e9e
kilian-nagel <kilian.nagel@etu.umontpellier.fr>
2024-02-12 18:59:04 +0200

    refactor: corrections des bugs et inconsistences de la navbar mobile

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,16 +1,23 @@
 
 const linksClassName = "nav-link";
-const linksData = [
-    { textContent: "biographie",id: "lien-biographie",href:"#biographie"},
-    { textContent: "projets",id:"lien-projets",href:"#projects"},
-    { textContent: "projet professionnel",id:"lien-professionnel",href:"./pages/projet-professionnel.html"},
-    { textContent: "contact",id:"lien-contact",href:"mailto:nagelkilian05@gmail.com"},
+const mobileLinks = [
+    { textContent: "biographie",id: "lien-biographie",href:"./index.html#biographie",color:"black"},
+    { textContent: "projets",id:"lien-projets",href:"./index.html#projects",color:"black"},
+    { textContent: "projet professionnel",id:"lien-professionnel",href:"./pages/projet-professionnel.html",color:"black"},
+    { textContent: "contact",id:"lien-contact",href:"mailto:nagelkilian05@gmail.com",color:"black"},
+]
+
+const normalLinks = [
+    { textContent: "biographie",href:"#biographie"},
+    { textContent: "projets",href:"#projects"},
+    { textContent: "projet professionnel",href:"./pages/projet-professionnel.html"},
+    { textContent: "contact",href:"mailto:nagelkilian05@gmail.com"},
 ]
 
 function buildNavbar() {
     const nav = buildElement("nav", { className: "nav" });
     const logo = buildLogo();
-    const navItems = buildNavItems();
+    const navItems = buildNavItems(normalLinks);
     const mailLink = buildMailLink();
     const mobileNavBtn = buildMobileNavIcon();
 
@@ -27,11 +34,15 @@ function buildLogo() {
     return logo;
 }
 
-function buildNavItems() {
+function buildNavItems(links) {
     const ul = buildElement("ul",{className:"nav-items"});
-    for(const link of linksData){
-        const liElement = buildElement("li",{className:"nav-item"})
-        const linkElement = buildElement("a",{...link,className:linksClassName});
+    for(const link of links){
+        const liElement = buildElement("li",{className:"nav-item"});
+        const linkElement = buildElement("a",{...link});
+        linkElement.addEventListener("click",()=>{
+            const fullscreenMenu = document.getElementById('fullscreenMenu');
+            fullscreenMenu.classList.remove("menu-open");
+        })
         liElement.appendChild(linkElement);
         ul.appendChild(liElement);
     }
@@ -44,7 +55,7 @@ function buildMailLink(){
 }
 
 function buildMobileNavIcon(){
-    const icon = buildElement("img",{id:"toggleMenu",style:"width: 20px;height:20px;display:none",src:"https://i.imgur.com/wFB9N9S.png",alt:"icone menu"});
+    const icon = buildElement("img",{id:"toggleMenu",style:"width: 20px;height:20px",src:"https://i.imgur.com/wFB9N9S.png",alt:"icone menu"});
     return icon
 }
 
@@ -54,23 +65,32 @@ function buildMobileNav(){
     const header = buildMobileNavHeader();
     const main = buildMobileNavMain();
     root.append(header,main);
+    return root;
 }
 
 function buildMobileNavHeader(){
-    const header = buildElement("div",{className:"header"});
+    const header = buildElement("div");
+    header.style = "display:flex;justify-content:space-between;padding:var(--spacing-md) var(--spacing-xl)";
+
     const logo = buildLogo();
+    logo.style.color = "black";
+
     const closeBtn = buildElement("img",{id:"hideMenuBtn",style:"width: 16px;height:16px",src:"https://i.imgur.com/OwAPDLJ.png",alt:"icone menu"});
     header.append(logo,closeBtn);
+    return header;
 }
 
 function buildMobileNavMain(){
     const div = buildElement("div",{className:"main"});
-    const title = buildElement("h3",{className:"fs-800"});
-    const items = buildNavItems();
-    div.append(title,items);
-    return div;
+    const items = buildNavItems(mobileLinks);
+    div.append(items);
+    return div; 
+}
+
+function changeElementsAttribute(){
+    changeAttributeOfElements();
 }
 
 const header = document.getElementById("header");
 const submenuWrapper = buildElement("div",{className:"submenu-wrapper"});
-header.append(buildNavbar(),submenuWrapper);
+header.append(buildNavbar(),buildMobileNav(),submenuWrapper);
