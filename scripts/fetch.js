ab7b09e603c573f258f8aa87a676c26f6472ad4a
kilian-nagel <kilian.nagel@etu.umontpellier.fr>
2024-02-14 00:53:41 +0200

    feat: requêtes pour les projets détaillés

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -17,6 +17,26 @@ function getShortProjectQuery(){
 return graphqlQuery;
 }
 
+function getLongProjectQuery(id){
+  const graphqlQuery = encodeURIComponent(`
+  {
+    posts (where: { id: "${id}" }) {
+      title
+      excerpt
+      tags
+      slug
+      typeOfProject
+      typeOfApplication
+      dates
+      content {
+        html
+      }
+    }
+  }
+`);
+return graphqlQuery;
+}
+
 async function getProjectsShortFormat(){
   const graphqlQuery = getShortProjectQuery();
   const urlWithQuery = `${apiUrl}?query=${graphqlQuery}`;
@@ -33,5 +53,24 @@ async function getProjectsShortFormat(){
   
   const rawData = await (await fetch(urlWithQuery, requestOptions)).json();
   const data = await rawData.data.posts;
+  return data;
+}
+
+async function getProjectsLongFormat(id){
+  const graphqlQuery = getLongProjectQuery(id);
+  const urlWithQuery = `${apiUrl}?query=${graphqlQuery}`;
+
+  const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${authToken}`,
+  };
+  
+  const requestOptions = {
+    method: 'GET',
+    headers: headers,
+  };
+  
+  const rawData = await (await fetch(urlWithQuery, requestOptions)).json();
+  const data = await rawData.data.posts[0];
   return data;
 }
