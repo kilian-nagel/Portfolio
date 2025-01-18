f928bd7fede3fd2d85b165053b178574d71223dd
nagelk <nagelkilian05@gmail.com>
2025-01-18 17:21:05 +0100

    Fix: lignes de codes utiles étaient commentées

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,16 @@
 import { Footer } from '@/components/footer/footer';
 import { Navbar } from '@/components/nav/navbar';
 
-//import "../public/styles/footer.css";
-//import "../public/styles/navbar.css";
-//import "../public/styles/normalize.css";
-//import "../public/styles/project-1.css";
-//import "../public/styles/projet-professionnel.css";
-//import "../public/styles/reset.css";
-//import "../public/styles/utility.css";
-//import "../public/styles/style.css";
-//import "../public/styles/variables.css";
-//import "../public/styles/output.css";
+import "@/styles/footer.css";
+import "@/styles/navbar.css";
+import "@/styles/normalize.css";
+import "@/styles/project-1.css";
+import "@/styles/projet-professionnel.css";
+import "@/styles/reset.css";
+import "@/styles/utility.css";
+import "@/styles/style.css";
+import "@/styles/variables.css";
+import "@/styles/output.css";
 
 export default function RootLayout({
     children
